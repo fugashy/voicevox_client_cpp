c70b644877cda0f7f3ccd4e00f9ce1e1314b89c5
fugashy <fugashy@icloud.com>
2024-05-25 13:19:02 +0900

    Fill doc of request.hpp

--- include/voicevox_client_cpp/request.hpp
+++ include/voicevox_client_cpp/request.hpp
@@ -9,11 +9,24 @@
 namespace voicevox_client_cpp::request
 {
 
+/**
+ * @brief A base class for a group of builder classes.
+ */
 class Builder
 {
 public:
+  /**
+   * @brief An alias of a pointer of this class
+   */
   using SharedPtr = std::shared_ptr<Builder>;
 
+  /**
+   * @brief Initialize URI
+   *
+   * @param path The URI
+   *
+   * @throws std::invalid_argument is thrown if the given URI is empty.
+   */
   Builder(const std::string& path) noexcept(false)
     : uri_builder_(path)
   {
@@ -21,30 +34,66 @@ public:
       throw std::invalid_argument("Builder requires a value represents resource name");
   }
 
+  /**
+   * @brief Create and provide a request based on the settings provided so far.
+   *
+   * @return The request
+   */
   web::http::http_request get() noexcept
   {
     req_.set_request_uri(uri_builder_.to_uri());
     return req_;
   }
 
+  /**
+   * @brief Set HTTP method to request.
+   *
+   * @param method HTTP method like:
+   *     web::http::method::GET,
+   *     web::http::methods::POST, ...
+   *
+   * @return The reference of this builder
+   */
   Builder& method(const web::http::method& method)
   {
     req_.set_method(method);
     return *this;
   }
 
+  /**
+   * @brief Set a header consists of a key and a value
+   *
+   * @param key The key of the header
+   * @param value The value of the header
+   *
+   * @return The reference of this builder
+   */
   Builder& header(const utility::string_t& key, const utility::string_t& value)
   {
     req_.headers().add(key, value);
     return *this;
   }
 
+  /**
+   * @brief Set the body of the request. (String type)
+   *
+   * @param body The request
+   *
+   * @return The reference of this builder
+   */
   Builder& body(const utility::string_t& body)
   {
     req_.set_body(body);
     return *this;
   }
 
+  /**
+   * @brief Set the body of the request. (JSON type)
+   *
+   * @param body The request
+   *
+   * @return The reference of this builder
+   */
   Builder& body(const web::json::value& body)
   {
     req_.set_body(body);
